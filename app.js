7ba138e585e7bd215f7bef522788a09c3a0ea3c0
Luis Alberto Alberca Presentacion <luisalbertoalberca@icloud.com>
2024-07-24 05:53:28 -0500

    animaciones del cursor

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+'use strict'
 
+$(document).ready(function(){
 
+    $('body').on({ 'mousemove' : function( e ){
+        console.clear()
+        let clientX = e.originalEvent.clientX
+        let clientY = e.originalEvent.clientY
+        $('#cursor').css({
+            'left' : (clientX - 40 ) + 'px',
+            'top' : (clientY - 40 ) + 'px'
+        })
+    }})
 
-<a href="https://www.freepik.es/foto-gratis/joven-estudiante-computadora_7870090.htm#fromView=search&page=2&position=1&uuid=9a8e152b-9848-4d7a-a2a7-4dff90227d46">Imagen de senivpetro en Freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/joven-mujer-sonriente-feliz-ropa-casual-portatil-aislado-pared-gris_8472822.htm#fromView=search&page=2&position=51&uuid=d3df8110-43c5-462c-940f-6970ab2d7c52">Imagen de diana.grytsku en Freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/mujer-rosa-bailando-lentejuelas-brillantes_3453859.htm#fromView=search&page=1&position=52&uuid=05e757ff-fb04-4e71-b695-ee6c700ed4bb">Imagen de freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/retrato-hombre-efectos-visuales-luces-azules_26813741.htm#fromView=search&page=3&position=2&uuid=05e757ff-fb04-4e71-b695-ee6c700ed4bb">Imagen de freepik</a>
-
-<a href="https://www.freepik.es/foto-gratis/hombre-joven-sensible-pensando_28258292.htm#fromView=search&page=4&position=35&uuid=05e757ff-fb04-4e71-b695-ee6c700ed4bb">Imagen de freepik</a>
+    $('.a').on({
+        'mouseover' : function(){
+            $('#cursor').addClass('mini')
+        },
+        'mouseout' : function(){
+            $('#cursor').removeClass('mini')
+        },
+    })
+})
